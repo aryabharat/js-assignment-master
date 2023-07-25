ad924a77600269f81a6cba8e3767a61a3e3f7613
aryabharat <aryabharat33@gmail.com>
2023-07-25 19:54:58 +0530

    modified getMatchesByTourName query

--- src/models/tour.js
+++ src/models/tour.js
@@ -1,18 +1,33 @@
-const mysql = require('../lib/mysql');
+const mysql = require("../lib/mysql");
 
 const getAllTours = async () => {
-    const statement = 'select * from tours;';
-    const parameters = [];
-    return await mysql.query(statement, parameters);
-}
+  const statement = "select * from tours;";
+  const parameters = [];
+  return await mysql.query(statement, parameters);
+};
 
-const getMatchesByTourName = async params => {
-    const statement = 'select * from matches left join tours on matches.tourId = tours.id where tours.name = ?';
-    const parameters = [ params.name ];
-    return await mysql.query(statement, parameters);
-}
+const getTourById = async (id) => {
+  const statement = "SELECT id as tourId, sportId FROM tours WHERE id = ?;";
+  const parameters = [id];
+  return await mysql.query(statement, parameters);
+};
+
+const getMatchesByTourName = async (name, limit, offset) => {
+  /**
+   *  - This is assuming that a tour will likely to have more then 1k matches in general
+   * - We can use ORM instead of raw query
+   */
+  const statement = `
+    SELECT * FROM
+    (SELECT * FROM tours WHERE tours.name = ? LIMIT 1)  tour
+    LEFT JOIN matches
+    ON matches.tourId = tour.id  LIMIT ? OFFSET ?`;
+  const parameters = [name, limit, offset];
+  return await mysql.query(statement, parameters);
+};
 
 module.exports = {
-    getAllTours: getAllTours,
-    getMatchesByTourName: getMatchesByTourName
-}
+  getAllTours: getAllTours,
+  getMatchesByTourName: getMatchesByTourName,
+  getTourById: getTourById,
+};
