bdd28138d195ed20b39d578acff1d55d8a773192
aryabharat <aryabharat33@gmail.com>
2023-07-25 19:55:16 +0530

    modified getAllSportsToursAndMatches query

--- src/models/sport.js
+++ src/models/sport.js
@@ -1,13 +1,14 @@
-const mysql = require('../lib/mysql');
+const mysql = require("../lib/mysql");
 
 const getAllSportsToursAndMatches = async () => {
-    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName ' +
-        'from matches m left join tours t on m.tourId = t.id ' +
-        'left join sports s on t.sportId = s.id';
-    const parameters = [];
-    return await mysql.query(statement, parameters);
-}
+  const statement =
+    "select s.name as sportName, t.name as tourName, m.name as matchName  m.id as matchId, m.startTime as startTime, m.format as format " +
+    "from matches m left join tours t on m.tourId = t.id " +
+    "left join sports s on t.sportId = s.id";
+  const parameters = [];
+  return await mysql.query(statement, parameters);
+};
 
 module.exports = {
-    getAllSportsToursAndMatches: getAllSportsToursAndMatches
-}
+  getAllSportsToursAndMatches: getAllSportsToursAndMatches,
+};
